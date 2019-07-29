f62b807a1a43801ae56d783143fc282fb058f8b8
liuy <liuyuit@aliyun.com>
2019-07-29 23:56:41 +0800

    bat批处理上传

--- StudyCode/Graph/Graph.php
+++ StudyCode/Graph/Graph.php
@@ -33,7 +33,59 @@ class Graph
 {
     private $V = 0; // 顶点数目
     private $E = 0; // 边的数目
-    public $adg;// 邻接表
+    public $adg = [];// 邻接表
+
+
+    public function initGraph($V){
+        $this->V = $V;
+        $this->E = 0;
+        $this->adg = [];
+
+        for ($i = 0; $i < $V; $i++){
+            $this->adg[$i] = [];
+        }
+    }
+
+    /**
+     * @param $V
+     * @param $edgeList // 每个元素是一个包含两个顶点的数组
+     */
+    public function createGraph($V, $edgeList){
+        $this->initGraph($V);
+
+        foreach ($edgeList as $edge){
+            $this->addEdge($edge[0], $edge[1]);
+        }
+    }
+
+    public function V(){
+        return $this->V;
+    }
+
+    public function E(){
+        return $this->E;
+    }
+
+    /**
+     * 添加一条边
+     * @param $v // 顶点
+     * @param $w // 顶点
+     */
+    public function addEdge($v, $w){
+        $this->adg[$v][] = $w;
+        $this->adg[$w] = $v;
+        $this->E++;
+    }
+}
+
+/**
+ * 图
+ */
+class MyGraph
+{
+    private $V = 0; // 顶点数目
+    private $E = 0; // 边的数目
+    public $adg = [];// 邻接表
 
 
     public function initGraph($V){
@@ -79,3 +131,4 @@ class Graph
         $this->E++;
     }
 }
+
