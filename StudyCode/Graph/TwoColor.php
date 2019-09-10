1ec60a7a07b80c36350ea17db8fb85fbd938ff1c
liuy <liuyuit@aliyun.com>
2019-09-10 23:56:58 +0800

    bat批处理上传

--- StudyCode/Graph/TwoColor.php
+++ StudyCode/Graph/TwoColor.php
@@ -67,7 +67,7 @@ class TwoColor
 
     public function dfs(Graph $graph, int $v){
         $this->marked[$v] = true; // 将该顶点标记为已访问
-        
+
 
         $adgVertexes = $graph->adg($v);
         foreach ($adgVertexes as $adgVertex){
@@ -77,12 +77,12 @@ class TwoColor
             }elseif ($adgVertex ){
                 // 遍历到了一个已被标记的顶点，并且这个顶点不是上一个递归访问的顶点，
                 // 这个顶点在两条路线上被遍历到，所以这是个有环图
-                $this->hasCycle = true;
+                $this->isTwoColorable = true;
             }
         }
     }
 
-    public function hasCycle(){
+    public function isBipartite(){
         return $this->isTwoColorable;
     }
 }
