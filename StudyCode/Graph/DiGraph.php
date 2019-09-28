fbede350e72b54008496d7ae60da10f38e9c7004
liuy <liuyuit@aliyun.com>
2019-09-28 19:33:27 +0800

    bat批处理上传

--- StudyCode/Graph/DiGraph.php
+++ StudyCode/Graph/DiGraph.php
@@ -7,9 +7,9 @@ use Sort\Queue;
 
 require_once '../Sort/Queue.php';
 
-// graphExample();
+ digraphExample();
 
-function graphExample()
+function digraphExample()
 {
     $v = 5;
     $array = [
@@ -30,7 +30,7 @@ function graphExample()
 /**
  * 图
  */
-class Graph
+class DiGraph
 {
     private $V = 0; // 顶点数目
     private $E = 0; // 边的数目
@@ -83,76 +83,3 @@ class Graph
         return $this->adg[$vertex];
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
- * 图
- */
-class MyGraph
-{
-    private $V = 0; // 顶点数目
-    private $E = 0; // 边的数目
-    public $adg = [];// 邻接表
-
-
-    public function initGraph($V){
-        $this->V = $V;
-        $this->E = 0;
-        $this->adg = [];
-
-        for ($i = 0; $i < $V; $i++){
-            $this->adg[$i] = New Queue();
-        }
-    }
-
-    /**
-     * @param $V
-     * @param $edgeList // 每个元素是一个包含两个顶点的数组
-     */
-    public function createGraph($V, $edgeList){
-        $this->initGraph($V);
-
-        foreach ($edgeList as $edge){
-            $this->addEdge($edge[0], $edge[1]);
-        }
-    }
-
-    public function V(){
-        return $this->V;
-    }
-
-    public function E(){
-        return $this->E;
-    }
-
-    /**
-     * 添加一条边
-     * @param $v // 顶点
-     * @param $w // 顶点
-     */
-    public function addEdge($v, $w){
-        /** @var Queue $vQueue */
-        $vQueue = $this->adg[$v];
-        $vQueue->enQueue($w);
-        /** @var Queue $wQueue */
-        $wQueue = $this->adg[$w];
-        $wQueue->enQueue($v);
-        $this->E++;
-    }
-}
-
