c42fe01ec114247b419be22810c8bb89614e2458
liuy <liuyuit@aliyun.com>
2019-10-04 11:09:26 +0800

    bat批处理上传

---
+++ StudyCode/Graph/DirectedDFS.php
@@ -0,0 +1,97 @@
+<?php
+namespace Graph;
+
+ini_set("display_errors", "On");
+ini_set("html_errors", "On");
+//use Sort\Queue;
+
+//require_once '../Sort/Queue.php';
+
+directedDFSExample();
+
+function directedDFSExample()
+{
+    $v = 5;
+    $array = [
+        [0, 1],
+        [1, 2],
+        [2, 3],
+        [3, 4],
+    ];
+
+    $graph = new Graph($v, $array);
+
+    echo '<pre>';
+    print_r($graph->adg);
+    echo '<pre>';
+}
+
+
+/**
+ * 图
+ */
+class DirectedDFS
+{
+    private $V = 0; // 顶点数目
+    private $E = 0; // 边的数目
+    public $adg = [];// 邻接表
+
+
+    /**
+     * @param $V
+     * @param $edgeList array 每个元素是一个包含两个顶点的数组
+     */
+//    public function __construct($edgeList, $V){
+//        $this->initGraph($V);
+//
+//        foreach ($edgeList as $edge){
+//            $this->addEdge($edge[0], $edge[1]);
+//        }
+//    }
+
+    public function __construct($V){
+        $this->V = $V;
+        $this->E = 0;
+        $this->adg = [];
+
+        for ($i = 0; $i < $V; $i++){
+            $this->adg[$i] = [];
+        }
+    }
+
+
+    public function V(){
+        return $this->V;
+    }
+
+    public function E(){
+        return $this->E;
+    }
+
+    /**
+     * 添加一条边
+     * @param $v // 顶点
+     * @param $w // 顶点
+     */
+    public function addEdge($v, $w){
+        $this->adg[$v][] = $w;
+//        $this->adg[$w][] = $v;
+        $this->E++;
+    }
+
+    public function adg(int $vertex){
+        return $this->adg[$vertex];
+    }
+
+    public function reverse(){
+        $reverseDigraph = new DiGraph($this->V());
+        for ($i = 0; $i <= $reverseDigraph->V(); $i++){
+            $adgVertexes = $this->adg($i);
+            foreach ($adgVertexes as $adgVertex){
+                $reverseDigraph->addEdge($adgVertex, $i);
+            }
+        }
+
+        return $reverseDigraph;
+    }
+}
