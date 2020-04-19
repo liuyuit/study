bcac5da5270a7b59cb36632bb697f35ca1f198a2
liuyu <liuyuit@aliyun.com>
2020-04-20 00:02:33 +0800

    shell script auto push

---
+++ Other/Script/sh/auto_push_git_repositories.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+pwd="/Users/liuyu/Documents/git/"       #初始化目录
+for category_dir in $(ls $pwd); do    # 循环 git 仓库根目录
+ cd $pwd;
+ for dir in $(ls $category_dir); do # 循环 git 分类目录
+   cd $category_dir; 
+   cd $dir;
+    git pull;
+    git add .
+    git commit -m 'shell script auto push';
+    git push;
+    cd ../; 
+    cd ../; 
+  done
+done
