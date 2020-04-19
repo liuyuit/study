def2496c43d571f0fea6dc4110af7187e3ea86bb
liuyu <liuyuit@aliyun.com>
2020-04-20 00:26:58 +0800

    shell script auto push

--- Other/Script/sh/auto_push_git_repositories.sh
+++ Other/Script/sh/auto_push_git_repositories.sh
@@ -1,10 +1,18 @@
 #/bin/sh
 pwd="/Users/liuyu/Documents/git/"       #初始化目录
 for category_dir in $(ls $pwd); do    # 循环 git 仓库根目录
- cd $pwd;
- for dir in $(ls $category_dir); do # 循环 git 分类目录
-   cd $category_dir; 
-   cd $dir;
+  if [$category_dir == 'code'];then  # 代码相关的的 git 仓库不自动推送拉取
+    continue
+  fi
+  
+  if [$category_dir == 'unauthorized'];then  # 没有推送权限的仓库不自动推送拉取
+    continue
+  fi
+
+  cd $pwd;
+  for dir in $(ls $category_dir); do # 循环 git 分类目录
+    cd $category_dir; 
+    cd $dir;
     git pull;
     git add .
     git commit -m 'shell script auto push';
