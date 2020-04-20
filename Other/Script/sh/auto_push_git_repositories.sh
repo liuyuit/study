807ca160e492edf74376510c5a357d40616a1755
liuyu <liuyuit@aliyun.com>
2020-04-20 09:54:36 +0800

    shell script auto push

--- Other/Script/sh/auto_push_git_repositories.sh
+++ Other/Script/sh/auto_push_git_repositories.sh
@@ -1,16 +1,16 @@
 #/bin/sh
-pwd="/Users/liuyu/Documents/git/"       #初始化目录
-for category_dir in $(ls $pwd); do    # 循环 git 仓库根目录
-  code_dir=`ls $pwd | grep code`;
-  if ["$category_dir" = "$code_dir"];then  # 代码相关的的 git 仓库不自动推送拉取
+start_dir="/Users/liuyu/Documents/git/"       #初始化目录
+for category_dir in $(ls $start_dir); do    # 循环 git 仓库根目录
+  code_dir=`ls $start_dir | grep code`;
+  if [ "$category_dir" = "$code_dir" ];then  # 代码相关的的 git 仓库不自动推送拉取
     continue
   fi
-  unauthorized_dir="unauthorized"
-  if ["$category_dir" = "$unauthorized_dir"];then  # 没有推送权限的仓库不自动推送拉取
+  unauthorized_dir=`ls $start_dir | grep unauthorized`
+  if [ "$category_dir" = "$unauthorized_dir" ];then  # 没有推送权限的仓库不自动推送拉取
     continue
   fi
 
-  cd $pwd;
+  cd $start_dir;
   for dir in $(ls $category_dir); do # 循环 git 分类目录
     cd $category_dir; 
     cd $dir;
