f4a09dce52609c52f5dc59a7b8e8d424fe021c6b
liuyu <liuyuit@aliyun.com>
2020-05-29 00:45:25 +0800

    shell script auto push

--- StudyCode/Algorithms/String/Count.php
+++ StudyCode/Algorithms/String/Count.php
@@ -5,6 +5,13 @@ class Count
 {
     public function __construct($alphabets, $chars)
     {
+        $alph = new Alphabet();
+    }
+}
 
+class Alphabet
+{
+    public function __construct()
+    {
     }
 }
