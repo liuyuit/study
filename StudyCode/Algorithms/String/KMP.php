3cc547b12ec7ba6be93b65af241e39b4f79b16b4
liuyu <liuyuit@aliyun.com>
2020-10-22 17:59:01 +0800

    bat批处理上传

--- StudyCode/Algorithms/String/KMP.php
+++ StudyCode/Algorithms/String/KMP.php
@@ -5,8 +5,8 @@
  */
 
 function example(){
-    $pat = 'as';
-    $txt = 'ag112123asd11';
+    $pat = 'aacaa';
+    $txt = 'aabaacaaabraaca1';
     $kmp = new KMP($pat);
     $offset = $kmp->search($txt);
     var_dump($offset);
@@ -15,9 +15,15 @@ function example(){
 example();
 
 
+/**
+ * 用确定有限自动状态机的方式来做子字符串查找
+ * Class KMP
+ */
 class KMP
 {
     private $pat = '';
+    // [0 => [0 => 0, 1 => 1, 2 => 0]]  外面数组的索引表示在确定有限自动状态机中，下一个输入的字符的 ascii 码。
+    // [0 => 0, 1 => 1, 2 => 0] .里面数组的索引表示对于某一个输入的字符，如果当前状态是 key，那么输入这个字符后的状态是 value
     private $dfa = [];
 
     public function __construct($pat)
@@ -27,17 +33,19 @@ class KMP
         $M = strlen($pat);
         $R = 256;
         $this->dfa = $this->iniArray($R, $M);
-        $this->dfa[$this->pat[0]][0] = 1;
+        $this->dfa[static::charAt($this->pat, 0)][0] = 1; // 如果之前的状态是 0，那么输入 pat 的第一个字符后，状态将变为 1
 
         for ($X = 0, $j = 1; $j < $M; $j++){
             // 计算 dfa[][$j]
             for ($c = 0; $c < $R; $c++){
-                $this->dfa[$c][$j] = $this->dfa[$c][$X]; // 复制失败情况下的值
+                $this->dfa[$c][$j] = $this->dfa[$c][$X]; // 复制匹配失败情况下的值
             }
 
             $this->dfa[static::charAt($pat, $j)][$j] = $j + 1; // 设置匹配成功情况下的值
             $X = $this->dfa[static::charAt($pat, $j)][$X]; // 更新重启状态
         }
+
+        /*foreach ($this->dfa  as $key => $value){foreach ($value as $item) {    if ($item != 0){        echo $item;    }}}*/
     }
 
     public function search($txt){
@@ -45,7 +53,8 @@ class KMP
         $N = strlen($txt);
         $M = strlen($this->pat);
         for ($i = 0, $j = 0; $i < $N && $j <$M; $i++){
-            $j = $this->dfa[$txt[$i]][$j];
+            $ascii = static::charAt($txt, $i);
+            $j = $this->dfa[$ascii][$j];
         }
 
         if ($j == $M){
